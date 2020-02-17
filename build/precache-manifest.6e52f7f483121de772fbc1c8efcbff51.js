cfe0f4b9aac94819cb3db8cf7c077bffe11bc452
Daniel <ddelgadogomez@infomm.local>
2020-02-17 18:46:52 +0100

    Version 1.0 del github io ex 17-02-2020

---
+++ build/precache-manifest.6e52f7f483121de772fbc1c8efcbff51.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfe27690d0abc167f9dc2c228e199c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64fff7ced3ac001bca93",
+    "url": "./static/css/main.0af3a7e0.chunk.css"
+  },
+  {
+    "revision": "459e7760ad6787258715",
+    "url": "./static/js/2.33bcccdc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.33bcccdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fff7ced3ac001bca93",
+    "url": "./static/js/main.229d41e2.chunk.js"
+  },
+  {
+    "revision": "f18f341fb15cf7e67de2",
+    "url": "./static/js/runtime-main.356837ed.js"
+  },
+  {
+    "revision": "7e3ac7fa65a1c43aaf88f5b485ff2cd4",
+    "url": "./static/media/Paisaje1.7e3ac7fa.jpg"
+  },
+  {
+    "revision": "e9ef8e4260a780acb36c7b025ab12df6",
+    "url": "./static/media/Paisaje2.e9ef8e42.jpg"
+  },
+  {
+    "revision": "620d5e0e9adcc95e415f650eaf951e0c",
+    "url": "./static/media/Paisaje3.620d5e0e.jpg"
+  },
+  {
+    "revision": "b02dd1b692b66a8a07151390ce1d1c19",
+    "url": "./static/media/Paisaje4.b02dd1b6.jpg"
+  },
+  {
+    "revision": "12287c7f32eac56c2ddb199bb78b2336",
+    "url": "./static/media/Paisaje5.12287c7f.jpg"
+  },
+  {
+    "revision": "1b7a1f301f8bb6ce712e23901a16bbc9",
+    "url": "./static/media/Paisaje6.1b7a1f30.jpg"
+  },
+  {
+    "revision": "e8755a168efd26d31751ef573976af2d",
+    "url": "./static/media/Paisaje7.e8755a16.jpg"
+  },
+  {
+    "revision": "184df79ee30f62531cae563684f3eca7",
+    "url": "./static/media/Paisaje8.184df79e.jpg"
+  }
+]);
